cddf332462ecddb2621a7202c7e70252047b6fb0
majamala <maja.maric@fer.hr>
2018-05-25 15:35:00 +0200

    Porukica o preimenovanju.

--- src/main/java/com/yammer/SensorReadingDao.java
+++ src/main/java/com/yammer/SensorReadingDao.java
@@ -11,6 +11,6 @@ public interface SensorReadingDao {
     @SqlQuery("select * from sensorreadings;")
     public List<SensorReading> getSensorReadings();
 
-    @SqlQuery ("")
+    //@SqlQuery ("")
 
 }
